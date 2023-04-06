9d130d92be557326ac26f1e563b0bb80a3fd5f63
unknown <binhnguyen110120@gmail.com>
2023-04-06 17:30:00 +0700

    middleware  & bodyParser

--- apiRouter.js
+++ apiRouter.js
@@ -1,2 +1,19 @@
 const express = require("express");
-const routes = express.Router();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json("Router User GET ");
+});
+router.post("/", (req, res) => {
+  res.json("Router POST ");
+});
+
+router.put("/", (req, res) => {
+  res.json("Router PUT ");
+});
+
+router.delete("/", (req, res) => {
+  res.json("Router DELETE ");
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-import * as express from "express";
-// const express = require("express");
+// import * as express from "express";
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const post = 8080;
+const router = require("./apiRouter");
 
-app.get("/", (req, res) => {
-  res.json("Hello Express!");
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// const checkLoginAdmin = (req, res, next) => {
+//   if (Login) {
+//     req.user.role;
+//     next();
+//   }
+// };
+
+// const checkLogin = (req, res, next) => {
+//   if (Login) {
+//     req.user = user;
+//     next();
+//   } else {
+//     console.log("Please Login");
+//   }
+// };
+app.use("/api/v1/", router);
 
 app.listen(post, () => {
   console.log(`Example app listening at http://localhost:${post}`);
